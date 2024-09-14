d760484e2752f14109469fcde63a0215cefe5d65
Oluday <82329224+Oluday@users.noreply.github.com>
2024-09-14 15:23:50 +0100

    Update african_food_prices.py

--- african_food_prices.py
+++ african_food_prices.py
@@ -10,7 +10,6 @@ import plotly.express as px
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
 
-
 st.write(css, unsafe_allow_html=True)
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
