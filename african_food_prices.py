18cfefa62599a09c5299443e97deddf53d2d80f9
Oluday <82329224+Oluday@users.noreply.github.com>
2024-09-14 15:19:25 +0100

    Update african_food_prices.py

--- african_food_prices.py
+++ african_food_prices.py
@@ -87,7 +87,20 @@ st.markdown('###')
 
 page_bg_img = f"""
 <style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://i.postimg.cc/4xgNnkfX/Untitled-design.png");
+background-size: cover;
+background-position: center center;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
 
+div.vg-tooltip-element {
+display: none;
+}
 </style>
 """
 
