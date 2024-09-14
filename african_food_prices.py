4763adf3d68844bc4393684a8d4f9617403d8869
Oluday <82329224+Oluday@users.noreply.github.com>
2024-09-14 16:05:56 +0100

    Update african_food_prices.py

--- african_food_prices.py
+++ african_food_prices.py
@@ -43,7 +43,7 @@ def load_data():
     df['state'].fillna('unknown', inplace=True)
     #df['year'] = df.year.replace(',', '.')
     # Remove the comma from the name column
-    df['year'] = df['year'].str.replace(',', '',  inplace=True)
+    #df['year'] = df['year'].str.replace(',', '',  inplace=True)
     #renaming two columns
     df.rename({'um_unit_id': 'exchanged_qty'}, axis=1, inplace=True)
     df['exchanged_qty'] = df.exchanged_qty.astype('float')
