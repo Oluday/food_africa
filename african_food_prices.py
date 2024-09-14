2ef4262199afc5657b03f56708bdc932e558a63a
Oluday <82329224+Oluday@users.noreply.github.com>
2024-09-14 17:05:41 +0100

    Update african_food_prices.py

--- african_food_prices.py
+++ african_food_prices.py
@@ -8,8 +8,6 @@ import plotly.express as px
 
 # page config
 st.set_page_config(page_title="African Food Prices", layout='centered', page_icon='📊')
-#with open('style.css') as f:
-#    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 
 
